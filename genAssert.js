e22ee6dd8ae3f8786bac2730624f00d2d0bd5474
Jordy Alkema <jordy@alkema.co>
2021-10-18 19:01:21 +0200

    It works!

--- genAssert.js
+++ genAssert.js
@@ -8,7 +8,9 @@ highlightBlocks.forEach(block => {
     button.style.right = "2em";
     button.style.color = "red";
     button.addEventListener("click", () => {
-        groupStatements(button.parentElement.innerText);
+        let assertList = genAsserts(groupStatements(button.parentElement.innerText));
+        let formattedText = formatClipboardText(assertList);
+        navigator.clipboard.writeText(formattedText);
     });
     block.appendChild(button);
 })
@@ -18,9 +20,29 @@ const groupStatements = (text) => {
     let outList = [];
     text.split("\n").forEach(line => {
         if (inRegex.test(line)){
-            console.log(line);
+            inList.push(line.split(inRegex)[1]);
         } else if (outRegex.test(line)){
-            console.log(line)
+            outList.push(line.split(outRegex)[1]);
         }
     })
+    return {
+        inList,
+        outList
+    }
+}
+
+const genAsserts = (lines) => {
+    assertStatements = [];
+    lines.inList.forEach((line, index) => {
+        assertStatements.push(`assert ${line} == ${lines.outList[index]}`);
+    })
+    return assertStatements;
+}
+
+const formatClipboardText = (assertList) => {
+    let text = "";
+    assertList.forEach(line => {
+        text += `${line} \n`;
+    })
+    return text;
 }
